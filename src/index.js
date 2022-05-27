d02ebe5ef459cbd7394fed7f79bb80a12928c781
Vera Ho <vh71886@gmail.com>
2022-05-27 16:03:32 -0400

    changed background image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const DIMX = 800;
-    const DIMY = 600;
+    const DIMY = 528;
 
     // Canvas Setup
     const cvsBackgroundEl = document.getElementById("canvas-background");
@@ -19,17 +19,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // // testing
     const backgroundImage = new Image();
-    backgroundImage.src = "./assets/background_sprite_by_nickanater1.png";
+    backgroundImage.src = "./assets/duckhunt_transparent_nicepng.png";
     backgroundImage.onload = () => {
-        // draw half of sprite image. og: 2,580 x 3,230 
         // drawImage(source file, img start width, img start height, img width, image height, xpos, pos, dest width, dest height * 2??);
-        ctx_foreground.drawImage(backgroundImage, 0, 1615, 2580, 3230, 0, 0, DIMX, 2 * DIMY);
-        console.log("draw background");
+        ctx_foreground.drawImage(backgroundImage, 0, 0, 980, 655, 0, 0, DIMX, DIMY);
     }
-    // ctx_background.fillStyle = "purple";
-    // ctx_gameboard.fillStyle = "transparent";
-    // ctx_foreground.fillStyle = "transparent";
-    // ctx.forEach( (el) => el.fillRect(0, 0, DIMX, DIMY))
+    ctx_background.fillStyle = "skyblue";
+    ctx_background.fillRect(0, 0, DIMX, DIMY)
 })
 
 

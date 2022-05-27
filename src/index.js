95d616aa77882c80e12cef2c94167d6f43702b99
Vera Ho <vh71886@gmail.com>
2022-05-27 15:42:36 -0400

    added sprite background to canvas

--- src/index.js
+++ src/index.js
@@ -18,10 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     // // testing
-    ctx_background.fillStyle = "purple";
-    ctx_gameboard.fillStyle = "blue";
-    ctx_foreground.fillStyle = "transparent";
-    ctx.forEach( (el) => el.fillRect(0, 0, DIMX, DIMY))
+    const backgroundImage = new Image();
+    backgroundImage.src = "./assets/background_sprite_by_nickanater1.png";
+    backgroundImage.onload = () => {
+        // draw half of sprite image. og: 2,580 x 3,230 
+        // drawImage(source file, img start width, img start height, img width, image height, xpos, pos, dest width, dest height * 2??);
+        ctx_foreground.drawImage(backgroundImage, 0, 1615, 2580, 3230, 0, 0, DIMX, 2 * DIMY);
+        console.log("draw background");
+    }
+    // ctx_background.fillStyle = "purple";
+    // ctx_gameboard.fillStyle = "transparent";
+    // ctx_foreground.fillStyle = "transparent";
+    // ctx.forEach( (el) => el.fillRect(0, 0, DIMX, DIMY))
 })
 
 

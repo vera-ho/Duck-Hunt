039bdfe551151b15a9ec938b988e027906c1f305
Vera Ho <vh71886@gmail.com>
2022-06-01 17:33:05 -0400

    Testing deployment with sound

--- src/scripts/gameaudio.js
+++ src/scripts/gameaudio.js
@@ -1,10 +1,19 @@
 export default class GameAudio {
 
     constructor() {
-        this.duckFlap = new Audio("./../../assets/sound/duckhunt001.wav");
-        this.shoot = new Audio("./../../assets/sound/duckhunt002.wav");
-        this.introSound = new Audio("./../../assets/sound/duckhunt003.mp3");
+
+        // trying different paths to deploy with sound properly
+        this.duckFlap = new Audio("./assets/sound/duckhunt001.wav");
+        this.shoot = new Audio("./sound/duckhunt002.wav");
+        this.introSound = new Audio();
+        this.introSound.src = "./../../assets/sound/duckhunt003.mp3";
+        // this.duckFlap = new Audio("./../../assets/sound/duckhunt001.wav");
+        // this.shoot = new Audio("./../../assets/sound/duckhunt002.wav");
+        // this.introSound = new Audio("./../../assets/sound/duckhunt003.mp3");
         // this.bark = new Audio("../../assets/sound/duckhunt004.wav");
     }
 
 }
+
+// /duck-hunt/src/scripts/gameaudio.js
+// /duck-hunt/assets/sound/duckhunt001.wav

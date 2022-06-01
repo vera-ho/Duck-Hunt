6a7f92b9eed3b188b85736aede1ae7d85ec132c1
Vera Ho <vh71886@gmail.com>
2022-06-01 17:05:04 -0400

    testing audio paths

--- src/scripts/gameaudio.js
+++ src/scripts/gameaudio.js
@@ -1,9 +1,9 @@
 export default class GameAudio {
 
     constructor() {
-        this.duckFlap = new Audio("../../assets/sound/duckhunt001.wav");
-        this.shoot = new Audio("../../assets/sound/duckhunt002.wav");
-        this.introSound = new Audio("../../assets/sound/duckhunt003.mp3");
+        this.duckFlap = new Audio("./../../assets/sound/duckhunt001.wav");
+        this.shoot = new Audio("./../../assets/sound/duckhunt002.wav");
+        this.introSound = new Audio("./../../assets/sound/duckhunt003.mp3");
         // this.bark = new Audio("../../assets/sound/duckhunt004.wav");
     }
 

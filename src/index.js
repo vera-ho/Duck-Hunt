abe4038fc08d954425bf336dff8831e6ea3fee51
Vera Ho <vh71886@gmail.com>
2022-06-11 15:42:10 -0400

    Removes hunt listener when restarting

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ export const DIMX = 800;
 export const DIMY = 528;
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Canvas Setup
     const background = new Canvas("canvas-background", DIMX, DIMY);        
     const gameboard = new Canvas("canvas-gameboard", DIMX, DIMY);
     const foreground = new Canvas("canvas-foreground", DIMX, DIMY);
@@ -17,37 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
     background.setColor("skyblue");
     foreground.setImage(foregroundPath);
 
-    // Game
     let game = new Game(gameboard, foreground);
     let view = new GameView(game, foreground, gameboard);
-    // splashImage(foreground, gameboard, game);
     splashImage(view);
     soundToggleListener(game);
 })
 
-
-// TEST LISTENER
-
-// function test(canvas) {
-//     console.log("test function");
-//     document.addEventListener("click", testListener);
-//     let count = 0;
-
-//     function testListener(e) {
-//         console.log("test listener");
-//         console.log(e)
-//         console.log(canvas);
-//         count++;
-//         console.log(count)
-
-//         if(count === 5) {
-//             console.log("remove!")
-//             document.removeEventListener("click", testListener);
-//         }
-//     }
-// }
-
-// END TEST
 
 //********************   Listeners   ********************//
 function splashImage(view) {                     
@@ -61,126 +35,11 @@ function splashImage(view) {
             } else {
                 splashEl.remove();
                 clearInterval(fadeSplash);
-                // playButtonListener(foreground, gameboard, game);
                 view.start();
             }
         }, 100)
     }, { once: true })
 }
-
-// function playButtonListener(foreground, gameboard, game) {              // GameView file?
-//     let playButton = document.getElementById("play-button");
-//     playButton.addEventListener("click", () => {
-//         game.start();
-//         playButton.style.display = "none";
-
-//         if(game.soundOn) {
-//             let sound = new GameAudio();
-//             sound.introSound.play();
-//         }
-
-//         huntEventListener(foreground, game);
-//         pauseButtonListener(game);
-//         restartButtonListener(foreground, gameboard, game);
-//     }, { once: true });
-// }
-
-
-
-// function huntEventListener(foreground, game) {
-//     foreground.canvas.addEventListener("click", (e) => {
-//         if(game.soundOn) {
-//             let sound = new GameAudio();
-//             sound.shoot.play();
-//         }
-
-//         if(game.animating && !game.dogIntro) huntEvent(e, foreground, game);
-//     })
-// }
-
-// function pauseButtonListener(game) {
-//     let pauseButton = document.getElementById("pause-button") 
-//     pauseButton.addEventListener("click", () => {
-//         if(pauseButton.innerHTML === "Pause" && game.animating) {
-//             game.pause();
-//             pauseButton.innerHTML = "Resume";
-//         } else {
-//             pauseButton.innerHTML = "Pause";
-//             game.resume();
-//         }
-//     })
-// }
-
-// function restartButtonListener(foreground, gameboard, game) {
-//     let restartButton = document.getElementById("restart-button");
-
-//     restartButton.addEventListener("click", (e) => {
-//         game.stop();
-//         game.message.style.zIndex = "0";
-//         restart(foreground, gameboard, game);
-//         let playButton = document.getElementById("play-button");
-//         playButton.style.display = "block";
-//     })
-// }
-
-// function restart(foreground, gameboard, game) {
-//     foreground.canvas.removeEventListener("click", (e) => {  // not working
-//         huntEvent(e, foreground, game);
-//     })
-//     game.counterEl.style.zIndex = "0";
-//     gameboard.clear();
-//     game.restart();
-//     let newGame = new Game(gameboard, foreground);
-//     playButtonListener(foreground, gameboard, newGame);
-// }
-
-// // Player clicked on canvas to hunt
-// function huntEvent(e, foreground, game) {
-
-//     // Get click position
-//     let bound = foreground.canvas.getBoundingClientRect();
-//     let hit_x = e.clientX - bound.left;
-//     let hit_y = e.clientY - bound.top;
-
-//     for(let i = 0; i < game.duckArray.length; i++) {
-//         let duck = game.duckArray[i];
-
-//         // Set hit box
-//         let xUpBound = duck.pos[0] + duck.imgSize;
-//         let yUpBound = duck.pos[1] + duck.imgSize;
-//         let xLowBound = duck.pos[0];
-//         let yLowBound = duck.pos[1];
-
-//         // Adjust for reversed image (duck flying left)
-//         if(duck.vel[0] < 0) {
-//             xUpBound = duck.pos[0];
-//             xLowBound = duck.pos[0] - duck.imgSize;
-//         }
-
-//          //successful hunt
-//         if(hit_x < xUpBound && hit_x > xLowBound && 
-//            hit_y < yUpBound && hit_y > yLowBound) {
-
-//             // Stop flying animation
-//             console.log("Hit!")
-//             duck.vel = [0, 0];
-//             duck.flying = false;
-//             game.score++; 
-//             game.hit = true;
-//             break;
-
-//         } else { // failed hunt
-//             // console.log("Miss! Haha.")
-//             // animate laughing dog
-
-//         }
-//     }
-
-//     if(!game.hit && game.ammo > 0 && game.animating) {
-//         console.log("Miss! Haha.")
-//         game.ammo--;
-//     } 
-// }
 
 function soundToggleListener(game) {
     let toggle = document.getElementById("sound-effects");
--- src/scripts/gameview.js
+++ src/scripts/gameview.js
@@ -1,4 +1,5 @@
 import Game from "./game";
+import GameAudio from "./gameaudio";
 
 export default class GameView {
     constructor(game, foreground, gameboard) {
@@ -13,125 +14,139 @@ export default class GameView {
     }
 
     start() {
-        
+        this.playButtonListener();
     }
 
     playButtonListener() {
         this.playButton.addEventListener("click", () => {
+            let sound = new GameAudio();
             if(this.game.soundOn) {
-                let sound = new GameAudio();
                 sound.introSound.play();
             } else {
                 if(!sound) return;
-                sound.pause();
+                sound.introSound.pause();
             }
 
             this.game.start();
             this.playButton.style.display = "none";
 
-            huntEventListener(foreground, game);
-            pauseButtonListener(game);
-            restartButtonListener(foreground, gameboard, game);
-    }, { once: true });
+            this.huntEventListener();
+            this.pauseButtonListener();
+            this.restartButtonListener();
+        }, { once: true });
     }
-}
 
+    huntEventListener() {
+        let canvas = this.foreground.canvas;
+        canvas.addEventListener("click", huntEvent)
+        let count = 0;
+        let game = this.game;
 
-
-
-
-
-function huntEventListener(foreground, game) {
-    foreground.canvas.addEventListener("click", (e) => {
-        if(game.soundOn) {
+        function stopListener() {
+            console.log('stop listener');
+            canvas.removeEventListener("click", huntEvent)
+        }
+        this.huntEventListener.stopListener = stopListener;
+    
+        function huntEvent(e) {
             let sound = new GameAudio();
-            sound.shoot.play();
-        }
-
-        if(game.animating && !game.dogIntro) huntEvent(e, foreground, game);
-    })
-}
-
-function pauseButtonListener(game) {
-    let pauseButton = document.getElementById("pause-button") 
-    pauseButton.addEventListener("click", () => {
-        if(pauseButton.innerHTML === "Pause" && game.animating) {
-            game.pause();
-            pauseButton.innerHTML = "Resume";
-        } else {
-            pauseButton.innerHTML = "Pause";
-            game.resume();
-        }
-    })
-}
-
-function restartButtonListener(foreground, gameboard, game) {
-    let restartButton = document.getElementById("restart-button");
-
-    restartButton.addEventListener("click", (e) => {
-        game.stop();
-        game.message.style.zIndex = "0";
-        restart(foreground, gameboard, game);
-        let playButton = document.getElementById("play-button");
-        playButton.style.display = "block";
-    })
-}
-
-function restart(foreground, gameboard, game) {
-    foreground.canvas.removeEventListener("click", (e) => {  // not working
-        huntEvent(e, foreground, game);
-    })
-    game.counterEl.style.zIndex = "0";
-    gameboard.clear();
-    game.restart();
-    let newGame = new Game(gameboard, foreground);
-    playButtonListener(foreground, gameboard, newGame);
-}
-
-// Player clicked on canvas to hunt
-function huntEvent(e, foreground, game) {
-
-    // Get click position
-    let bound = foreground.canvas.getBoundingClientRect();
-    let hit_x = e.clientX - bound.left;
-    let hit_y = e.clientY - bound.top;
-
-    for(let i = 0; i < game.duckArray.length; i++) {
-        let duck = game.duckArray[i];
-
-        // Set hit box
-        let xUpBound = duck.pos[0] + duck.imgSize;
-        let yUpBound = duck.pos[1] + duck.imgSize;
-        let xLowBound = duck.pos[0];
-        let yLowBound = duck.pos[1];
-
-        // Adjust for reversed image (duck flying left)
-        if(duck.vel[0] < 0) {
-            xUpBound = duck.pos[0];
-            xLowBound = duck.pos[0] - duck.imgSize;
-        }
-
-         //successful hunt
-        if(hit_x < xUpBound && hit_x > xLowBound && 
-           hit_y < yUpBound && hit_y > yLowBound) {
-
-            // Stop flying animation
-            console.log("Hit!")
-            duck.vel = [0, 0];
-            duck.flying = false;
-            game.score++; 
-            game.hit = true;
-            break;
-
-        } else { // failed hunt
-            // console.log("Miss! Haha.")
-            // animate laughing dog
-
+            if(game.soundOn) {
+                sound.shoot.play();
+            } else {
+                sound.shoot.pause();
+            }
+    
+            if(game.animating && !game.dogIntro) {
+                let bound = canvas.getBoundingClientRect();
+                let hit_x = e.clientX - bound.left;
+                let hit_y = e.clientY - bound.top;
+    
+                for(let i = 0; i < game.duckArray.length; i++) {
+                    let duck = game.duckArray[i];
+            
+                    // Set hit box
+                    let xUpBound = duck.pos[0] + duck.imgSize;
+                    let yUpBound = duck.pos[1] + duck.imgSize;
+                    let xLowBound = duck.pos[0];
+                    let yLowBound = duck.pos[1];
+            
+                    // Adjust for reversed image (duck flying left)
+                    if(duck.vel[0] < 0) {
+                        xUpBound = duck.pos[0];
+                        xLowBound = duck.pos[0] - duck.imgSize;
+                    }
+            
+                     //successful hunt
+                    if(hit_x < xUpBound && hit_x > xLowBound && 
+                       hit_y < yUpBound && hit_y > yLowBound) {
+            
+                        // Stop flying animation
+                        console.log("Hit!")
+                        duck.vel = [0, 0];
+                        duck.flying = false;
+                        game.score++; 
+                        game.hit = true;
+                        break;
+            
+                    } else { // failed hunt
+                        // animate laughing dog
+                    }
+                }
+            
+                if(!game.hit && game.ammo > 0 && game.animating) {
+                    console.log("Miss! Haha.")
+                    game.ammo--;
+                } 
+            }
         }
     }
 
-    if(!game.hit && game.ammo > 0 && game.animating) {
-        console.log("Miss! Haha.")
-        game.ammo--;
-    } 
+    pauseButtonListener() {
+        let game = this.game;
+        let pauseButton = document.getElementById("pause-button") 
+        pauseButton.addEventListener("click", buttonEvent)
+
+        function buttonEvent(e) {
+            if(pauseButton.innerHTML === "Pause" && game.animating) {
+                game.pause();
+                pauseButton.innerHTML = "Resume";
+            } else {
+                pauseButton.innerHTML = "Pause";
+                game.resume();
+            }
+        }
+    }
+
+    restartButtonListener() {
+        let game = this.game;
+        let gameboard = this.gameboard;
+        let foreground = this.foreground;
+        let huntEventListener = this.huntEventListener;
+        let playButton = this.playButton;
+        let newGame = this.newGame;
+
+        let restartButton = document.getElementById("restart-button");
+        restartButton.addEventListener("click", restart);
+
+        function restart() {
+            game.stop();
+            game.message.style.zIndex = "0";
+
+            huntEventListener.stopListener();
+            restartButton.removeEventListener("click", restart);
+
+            game.counterEl.style.zIndex = "0";
+            gameboard.clear();
+            game.restart();
+
+            playButton.style.display = "block";
+            newGame(foreground, gameboard);
+        }
+    }
+
+    newGame(foreground, gameboard) {
+        let newGame = new Game(gameboard, foreground);
+        let view = new GameView(newGame, foreground, gameboard);
+        view.start();
+    }
 }
